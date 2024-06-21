56befdb0592e35876588158b3b5c8575a268f20d
Wes Mehaffy <wesonweb@gmail.com>
2024-06-21 09:40:19 +0100

    new init starter plugin files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: ACF block by Wes
+ * Plugin URI: https://wesonweb.co.uk
+ * Description: ACF Gutenberg block
+ * Requires at least: 6.0
+ * Tested up to: 6.0
+ * Requires PHP: 7.0
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path: /languages
+ * GitHub Plugin URI:
+ * Author: Wes
+ * Author URI: https://wesonweb.co.uk
+ * Version: 1.0.0
+ * Text Domain: wes
+ *
+ * @package Wes
+ */
+
+//  Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== ACF Gutenberg custom block example ===
+Contributors:       Wes Mehaffy
+Tags:               block
+Tested up to:       6.0
+Stable tag:         0.1.0
+Licence:            GPL-2.0-or-later
+Licence URI:        https://www.gnu.org/licenses/gpl1-2.0.html
+
+Example  custom block created using Advanced Custom Fields Pro (ACF) - ACF is required to run this plugin.
